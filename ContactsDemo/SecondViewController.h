dc33406ea3643b3d3945b21ede8a0ba93e2e4456
Muhammad Hijazi <irekasoft@gmail.com>
2016-03-06 07:44:25 +0800

    Initial Commit

---
+++ ContactsDemo/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ContactsDemo
+//
+//  Created by Hijazi on 6/3/16.
+//  Copyright © 2016 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ ContactsDemo/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  ContactsDemo
+//
+//  Created by Hijazi on 6/3/16.
+//  Copyright © 2016 iReka Soft. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
